55a668cabc0afff5c0661d908b18e483f7184b28
shifatrojoni <shifatrojoni@gmail.com>
2023-11-18 16:25:43 +0600

    17/11/23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectComponent } from './project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+ { path:'',component:HomeComponent},
+ { path:'about',component:AboutComponent},
+ { path:'service',component:ServiceComponent},
+ { path:'contact',component:ContactComponent },
+ { path:'project',component:ProjectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
